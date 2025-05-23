2e51ee109be68daf1dc262b160ea5e918cb9a24d
Ondrej Laube <laube.ondrej@seznam.cz>
2025-05-23 18:14:11 +0200

    screen sice constants

--- src/pages/landing/components/Catalog.tsx
+++ src/pages/landing/components/Catalog.tsx
@@ -10,6 +10,10 @@ interface Props {
   data: undefined | DataItem[];
 }
 
+const XS = 24;
+const LG = 8;
+const XL = 6;
+
 function Catalog({ data }: Props) {
   return (
     <Row
@@ -26,14 +30,14 @@ function Catalog({ data }: Props) {
       >
         {!data
           ? Array.from({ length: 8 }).map((_, index) => (
-              <Col key={index} xs={24} lg={8} xl={6}>
+              <Col key={index} xs={XS} lg={LG} xl={XL}>
                 <Card>
                   <Skeleton active style={{ height: "260px" }} />
                 </Card>
               </Col>
             ))
           : data.map((item, index) => (
-              <Col key={index} xs={24} lg={8} xl={6}>
+              <Col key={index} xs={XS} lg={LG} xl={XL}>
                 <ProductCard index={index} item={item} />
               </Col>
             ))}
